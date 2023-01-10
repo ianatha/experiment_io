9af4e0c326bd88ea2ab4dd30a52b0c2699a80952
Ian Atha <ianatha@users.noreply.github.com>
2023-01-10 13:23:49 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,28 @@
-export default function() {
-  return "Hello, world!";
+// https://raw.githubusercontent.com/ianatha/experiment_io/main/index.ts
+
+import { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
+import zodToJsonSchema from "https://esm.sh/zod-to-json-schema@3.17.0";
+
+function input(objSchema) {
+  const schema = z.object(objSchema);
+  return $recv(zodToJsonSchema(schema));
+}
+
+function number(...args) {
+  return z.number(...args);
+}
+
+function string(...args) {
+  return z.string(...args);
+}
+
+function object(...args) {
+  return z.object(...args);
+}
+
+export {
+  input,
+  number,
+  string,
+  object,
 }
